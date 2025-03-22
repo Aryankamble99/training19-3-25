d62842a4d85324e645ff257c5471e38b3257cbee
Aryankamble99 <aryankamblern@gmail.com>
2025-03-22 11:03:07 +0530

    #8 - markdown Document Created

---
+++ module1.c
@@ -0,0 +1,7 @@
+#include "module1.h"
+#include<stdio.h>
+
+void module1_function()
+{
+    printf("\n This is Module 1");
+}
---
+++ module2.c
@@ -0,0 +1,7 @@
+#include"module2.h"
+#include<stdio.h>
+
+void module2_function()
+{
+    printf("\n This is Module 2");
+}
